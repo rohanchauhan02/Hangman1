19e438e991f894a0038c15f7df4195efa2ee7faf
Rohan Chauhan <rohanchauhan@Rohans-MacBook-Air.local>
2022-02-01 16:18:34 +0530

    01-02-2022

---
+++ script.js
@@ -0,0 +1,254 @@
+
+window.onload = function () {
+    var alphabet = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+        "-"
+    ];
+
+    var categories; //Array of topics
+    var chosenCategory; //Selected category
+    var getHint; //Word getHint
+    var word; //Selected word
+    var guess; //Geuss
+    var geusses = []; //Stored geusses
+    var lives; //lives
+    var counter; //Count correct geusses
+    var space; //Number of spaces in word "-"
+
+
+    //Get elements
+    var showLives = document.getElementById("mylives");
+    // var showCatagory = document.getElementById("scatagory"); 
+    // var getHint = document.getElementById("hint");
+    var showClue = document.getElementById("clue");
+    //create alphabet ul
+
+    var buttons = function () {
+        myButtons = document.getElementById("buttons");
+        letters = document.createElement("ul");
+        for (var i = 0; i < alphabet.length; i++) {
+            letters.id = "alphabet";
+            list = document.createElement("li");
+            list.id = "letter";
+            list.innerHTML = alphabet[i];
+            check();
+            myButtons.appendChild(letters);
+            letters.appendChild(list);
+        }
+    };
+    //Select Catagory
+
+    var selectCat = function () {
+        if (chosenCategory === categories[0]) {
+            catagoryName.innerHTML =
+                "The Chosen Category Is Premier League Football Teams";
+        } else if (chosenCategory == categories[1]) {
+            catagoryName.innerHTML = "The Chosen Category Is Films";
+        } else if (chosenCategory === categories[2]) {
+            catagoryName.innerHTML = "The Chosen Category Is cities";
+        }
+    };
+    //Create guesses ul
+    result = function () {
+        wordHolder = document.getElementById("hold");
+        correct = document.createElement("ul");
+        for (var i = 0; i < word.length; i++) {
+            correct.setAttribute("id", "my-word");
+            guess = document.createElement("li");
+            guess.setAttribute("class", "guess");
+            if (word[i] === "-") {
+                guess.innerHtml = "-";
+                space = 1;
+            } else {
+                guess.innerHTML = "_";
+            }
+            guesses.push(guess);
+            wordHolder.appendChild(correct);
+            correct.appendChild(guess);
+        }
+    };
+    //Show lives
+    comment = function () {
+        showLives.innerHTML = "You have " + lives + " lives";
+        if (lives < 1) {
+            showLives.innerHTML = "Game Overe";
+        }
+        for (var i = 0; i < guesses.length; i++) {
+            if (counter + space === guesses.length) {
+                showLives.innerHTML = "You Win!";
+            }
+        }
+    };
+    //Animate man
+    var animate = function () {
+        var drawMe = lives;
+        drawArray[drawMe]();
+    };
+    //Hangman
+    canvas = function () {
+        myStickman = document.getElementById("stickman");
+        context = myStickman.getContext("2d");
+        context.beginPath();
+        context.strokeStyle = "#fff";
+        context.lineWidth = 2;
+    };
+    head = function () {
+        myStickman = document.getElementById("stickman");
+        context = myStickman.getContext("2d");
+        context.beginPath();
+        context.arc(60, 25, 10, 0, Math.PI * 2, true);
+        context.stroke();
+    };
+
+    draw = function ($pathFromx, $pathFromy, $pathTox, $pathToy) {
+        context.moveTo($pathFromx, $pathFromy);
+        context.lineTo($pathTox, $pathToy);
+        context.stroke();
+    };
+    frame1 = function () {
+        draw(0, 150, 150, 150);
+    };
+    frame2 = function () {
+        draw(10, 0, 10, 600);
+    };
+    frame3 = function () {
+        draw(0, 5, 70, 5);
+    };
+    frame4 = function () {
+        draw(60, 5, 60, 15);
+    };
+    torso = function () {
+        draw(60, 36, 60, 70);
+    };
+    rightArm = function () {
+        draw(60, 46, 100, 50);
+    };
+    leftArm = function () {
+        draw(60, 46, 20, 50);
+    };
+    rightLeg = function () {
+        draw(60, 70, 100, 100);
+    };
+    leftLeg = function () {
+        draw(60, 70, 20, 100);
+    };
+    drawArray = [
+        rightLeg,
+        leftLeg,
+        rightArm,
+        leftArm,
+        torso,
+        head,
+        frame4,
+        frame3,
+        frame2,
+        frame1,
+    ];
+    //OnClick Function
+
+    check = function () {
+        list.onclick = function () {
+            var guess = this.innerHTML;
+            
+            this.setAttribute("class", "active");
+            this.onclick = null;
+            for (var i = 0; i < word.length; i++) {
+                if (word[i] === guess) {
+                    guesses[i].innerHTML = guess;
+                    counter += 1;
+                }
+            }
+            var j = word.indexOf(guess);
+            if (j === -1) {
+                lives -= 1;
+                comment();
+                animate();
+            } else {
+                comment();
+            }
+            console.log(guess)
+        };
+    };
+    //Play
+
+    play = function () {
+        categories = [
+            [
+                "everton",
+                "liverpool",
+                "swansea",
+                "chelsea",
+                "hull",
+                "manchester-city",
+                "newcastle-united",
+            ],
+            ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
+            ["manchester", "milan", "madrid", "amsterdam", "patna"],
+        ];
+        chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+        word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+        word = word.replace(/\s/g, "-");
+        console.log(word);
+        buttons();
+        guesses = [];
+        lives = 10;
+        counter = 0;
+        space = 1;
+        result();
+        comment();
+        selectCat();
+        canvas();
+    };
+    play();
+    //Hint
+    hint.onclick = function () {
+        hints = [
+            [
+                "Based in Mersyside",
+                "Based in mersyside",
+                "first",
+                "second",
+                "third",
+                "fourth",
+                "fifth",
+            ],
+            ["1", "2", "3", "4", "5"],
+            ["I", "II", "III", "IV", "V"],
+        ];
+        var catagoryIndex = categories.indexOf(chosenCategory);
+        var hintIndex = chosenCategory.indexOf(word);
+        showClue.innerHTML = "Clue:- " + hints[catagoryIndex][hintIndex];
+    };
+    document.getElementById("reset").onclick = function () {
+        correct.parentNode.removeChild(correct);
+        letters.parentNode.removeChild(letters);
+        showClue.innerHTML = "";
+        context.clearReact(0, 0, 400, 400);
+        play();
+    };
+};
